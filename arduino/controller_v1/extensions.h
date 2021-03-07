f564192daf075671ea95b3ba4d0a909218b6666e
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2021-03-07 12:19:25 +0530

    07/03/2021

--- arduino/controller_v1/extensions.h
+++ arduino/controller_v1/extensions.h
@@ -1,7 +1,7 @@
 // just use it to display or transfer or to string conversion but not to use for
 // math operation
 //#define round1dec(a) floorf(a*10)/10                       // round float to 1
-//decimal points
+// decimal points
 float round1dec(float a) {
   return floorf(a * 10) / 10; // round float to 1 decimal points
 }
